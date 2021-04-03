ba2ce8008162390712f4340b66dbdcb00bd70e1f
prashanth-ds <prashanthds258@gmail.com>
2021-04-03 16:03:47 +0530

    Initial commit

---
+++ Conversion.php
@@ -0,0 +1,130 @@
+<?php
+
+    include("Lib/simple_html_dom.php");
+
+
+    class Conversion {
+
+        function __construct()
+        {
+            $this->countries = array();
+            $this->c_countries = array();
+            $this->codes = array();
+            $this->currency = array();
+            $this->currencies = array();
+            $this->rate = array();
+            $this->base = "USD";
+        }
+
+        function economies()
+        {
+            /*
+                  We will scrape the top 10 Economies in the world here.
+            */
+
+            $counter = 0;
+            $html = file_get_html("https://www.investopedia.com/insights/worlds-top-economies/");
+            foreach ($html->find("table.mntl-sc-block-table__table tbody tr") as $val) {
+                if ($counter == 0) {
+                    $counter++;
+                    continue;
+                } else {
+                    array_push($this->countries, $val->find("td", 0)->plaintext);
+                    $counter++;
+                }
+            }
+
+        }
+
+
+        function notations()
+        {
+            /*
+                Here we will scrape the ISO Notations of Top 10 Economies that we previously scraped.
+            */
+//            $this->economies();
+            $html1 = file_get_html("https://docs.1010data.com/1010dataReferenceManual/DataTypesAndFormats/currencyUnitCodes.html");
+            foreach ($html1->find("table#topic_m4v_rt3_5r__table_k2t_fv3_5r tbody tr") as $val) {
+                foreach ($this->countries as $val1) {
+                    $country = $val->find("td", 0)->plaintext; // used to retrieve country name.
+                    if ($country == strtoupper($val1)) {
+                        array_push($this->codes, $val->find("td", 2)->plaintext); // used to retrieve iso country code.
+                        array_push($this->currency, $val->find("td", 1)->plaintext); // used to retrieve currency.
+                        array_push($this->c_countries, $val->find("td", 0)->plaintext);
+                    }
+                }
+            }
+
+            array_pop($this->codes); // used to pop one country's code which has united states in its name.
+            array_pop($this->currency); // same as above
+            array_pop($this->c_countries);
+
+            $temp = array();
+            foreach ($this->c_countries as $c_country) {
+                $local = strtolower($c_country);
+                array_push($temp, ucwords($local)); // since countries names are all in upperCase so we lower it first and keep it in title format for equality check.
+            }
+
+            $this->c_countries = $temp;
+            $temp = array();
+            $temp2 = array();
+            $temp3 = array();
+            for($i = 0 ; $i<10 ; $i++){
+                for ($j =0 ; $j<10 ; $j++){
+                    if($this->countries[$i] == $this->c_countries[$j]) {
+                        array_push($temp, $this->c_countries[$j]);
+                        array_push($temp2, $this->codes[$j]);
+                        array_push($temp3, $this->currency[$j]);
+                    }
+                }
+            }
+
+            $this->c_countries = $temp;
+            $this->codes = $temp2;
+            $this->currency = $temp3;
+            return array($temp, $temp2, $temp3);
+
+        }
+
+        function check_symbol($key)
+        {
+            foreach ($this->codes as $code)
+                if (($code == $key) && ($code != $this->base))
+                    return 1;
+        }
+
+        function retreive($base = 'USD')
+        {
+
+            $endpoint = 'latest';
+            $this->base = $base;
+            $access_key = '14d74c57f14c2e4f03b6f32a'; //<- ds258 ------ tmkoc -> daf75307b508720224bdf31a
+            $ch = curl_init('https://v6.exchangerate-api.com/v6/' . $access_key . "/" . $endpoint . "/" . $this->base);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+            // Store the data:
+            $json = curl_exec($ch);
+            curl_close($ch);
+
+            // Decode JSON response:
+            $exchangeRates = json_decode($json, true);
+
+            $i = 0;
+            foreach ($exchangeRates['conversion_rates'] as $key => $value) {
+                if ($this->check_symbol($key)) {
+                    $this->currencies[$i] = $key;
+                    $this->rate[$i] = $value;
+                    $i = $i + 1;
+                }
+            }
+
+            // append recieved base currency and rate to the currencies and rate arrays.
+            array_push($this->currencies, $this->base);
+            array_push($this->rate, 1);
+
+            return array($this->currencies, $this->rate);
+
+        }
+    }
+
+?>
---
+++ main.php
@@ -0,0 +1,161 @@
+<?php
+
+    include ("Conversion.php");
+
+
+
+    if(isset($_POST['submit'])){
+        $value = $_POST['amount'];
+        $type = $_POST['currency'];
+
+        $data = new Conversion();
+        $data->economies();
+        $inputs = $data->notations();
+        $received = $data->retreive($type);
+    }
+    else{
+        $data = new Conversion();
+        $data->economies();
+        $inputs = $data->notations();
+        $received = $data->retreive();
+        $value = 1;
+        $type = "USD";
+    }
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Currencies</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <link href='https://fonts.googleapis.com/css?family=Coiny' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Josefin Sans' rel='stylesheet'>
+
+    <style>
+
+        #bg-color{
+            background-image: url("background.jpg");
+            background-repeat: no-repeat;
+            /*background-attachment: fixed;*/
+            background-size: 100% 100%;
+        }
+
+    </style>
+
+</head>
+<body id="bg-color">
+    <div class="topnav-centered">
+        <h3 class="h3 " style="font-family:Coiny; font-size: 55px; text-align: center; color: black; padding-top: 10px ">
+            Currency Calculator
+        </h3>
+    </div>
+
+
+<div class="container">
+    <form action="" method="POST">
+        <div class="form-row align-items-center">
+            <div class="col">
+                <label for="amt" style="padding: 15px; font-family: Josefin Sans; color: midnightblue">Value : </label>
+                <input type="text" style="padding-right: 25px" name="amount" id="amt" placeholder="<?php echo $value; ?>" value="<?php echo $value; ?>">
+            </div>
+            <div class="col">
+                <label for="change" style="padding: 15px; font-family: Josefin Sans; color: midnightblue">Currency : </label>
+                <select name="currency" id="change" style="padding-right: 25px">
+                    <option value="USD"
+                        <?php
+                            if($type == "USD")
+                                echo "selected=\"selected\"";
+                        ?>>United States (US Dollar - USD)</option>
+                    <option value="CNY"
+                        <?php
+                        if($type == "CNY")
+                            echo "selected=\"selected\"";
+                        ?>>China (Yuan Renminbi - CNY)</option>
+                    <option value="JPY"
+                        <?php
+                        if($type == "JPY")
+                            echo "selected=\"selected\"";
+                        ?>>Japan (Yen - JPY)</option>
+                    <option value="EUR"
+                        <?php
+                        if($type == "EUR")
+                            echo "selected=\"selected\"";
+                        ?>>Germany (Euro - EUR)</option>
+                    <option value="INR"
+                        <?php
+                        if($type == "INR")
+                            echo "selected=\"selected\"";
+                        ?>>India (Indian Rupee - INR)</option>
+                    <option value="GBP"
+                        <?php
+                        if($type == "GBP")
+                            echo "selected=\"selected\"";
+                        ?>>United Kingdom (Pound Sterling - GBP)</option>
+                    <option value="EUR"
+                        <?php
+                        if($type == "EUR")
+                            echo "selected=\"selected\"";
+                        ?>>France (Euro - EUR)</option>
+                    <option value="EUR"
+                        <?php
+                        if($type == "EUR")
+                            echo "selected=\"selected\"";
+                        ?>>Italy (Euro - EUR)</option>
+                    <option value="BRL"
+                        <?php
+                        if($type == "BRL")
+                            echo "selected=\"selected\"";
+                        ?>>Brazil (Brazilian Real - BRL)</option>
+                    <option value="CAD"
+                        <?php
+                        if($type == "CAD")
+                            echo "selected=\"selected\"";
+                        ?>>Canada (Canadian Dollar - CAD)</option>
+                </select>
+            </div>
+            <div class="col" style="padding: 12px; font-family: Josefin Sans; color: midnightblue">
+                <input type="submit" class ="btn btn-outline-dark" value="Submit" name="submit" style="">
+            </div>
+        </div>
+
+        <div style="padding: 10px 0px 12px 0px; font-family: Josefin Sans ;">
+            <h2>Top 10 Economies In World</h2>
+        </div>
+
+        <table id="example" class="table table-bordered table-hover table-striped  table-responsive table-dark " style="width:100%">
+            <thead>
+                <tr>
+                    <th>Sl.No</th>
+                    <th>Country</th>
+                    <th>Currency</th>
+                    <th>Conversion</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    $index = 1;
+                    for ($i =0 ; $i<10 ; $i++){
+                        echo "<tr><td>".$index."</td><td>".$inputs[0][$i]."</td>
+                                <td>".$inputs[2][$i]."</td>";
+//                        <td>".$inputs[2][$i]." (".$inputs[1][$i].")"."</td>
+                        for($j=0 ; $j<8 ; $j++){
+                            if($inputs[1][$i] == $received[0][$j]){
+                                echo "<td>".round((float)$value * $received[1][$j], 3)." ".$inputs[1][$i]."</td></tr>";
+                                break;
+                            }
+                        }
+                        $index++;
+                    }
+                ?>
+            </tbody>
+        </table>
+
+    </form>
+</div>
+
+</body>
+</html>
